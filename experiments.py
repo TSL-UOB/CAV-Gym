49ba0a421774eca36705f87cb7ca8550aee51582
Kevin McAreavey <>
2020-09-23 18:20:01 +0100

    Setup for additional experiments on valid initial positions
    Added setup for (parallelised) experiments on valid initial positions using optimised parameters.

--- experiments.py
+++ experiments.py
@@ -8,15 +8,15 @@ from config import Config, PedestriansConfig, HeadlessConfig, QLearningConfig, F
 from simulation import Simulation
 
 
-def make_agent_config(agent_type, log_dir, **kwargs):
+def make_agent_config(agent_type, log_dir):
     if agent_type is AgentType.RANDOM:
-        return RandomConfig(**kwargs)
+        return RandomConfig(epsilon=0.01)
     elif agent_type is AgentType.RANDOM_CONSTRAINED:
-        return RandomConstrainedConfig(**kwargs)
+        return RandomConstrainedConfig(epsilon=0.5)
     elif agent_type is AgentType.PROXIMITY:
-        return ProximityConfig(**kwargs)
+        return ProximityConfig(threshold=M2PX * 34)
     elif agent_type is AgentType.ELECTION:
-        return ElectionConfig(**kwargs)
+        return ElectionConfig(threshold=M2PX * 34)
     elif agent_type is AgentType.Q_LEARNING:
         return QLearningConfig(
             alpha=0.18,
@@ -36,8 +36,8 @@ def make_agent_config(agent_type, log_dir, **kwargs):
         raise NotImplementedError
 
 
-def make_config(agent_type, threshold):
-    log_dir = f"logs/agent_type={agent_type}/threshold={threshold}"
+def make_config(agent_type, outbound_pavement, inbound_pavement):
+    log_dir = f"logs/agent_type={agent_type}/outbound_pavement={outbound_pavement}/inbound_pavement={inbound_pavement}"
     return log_dir, Config(
         verbosity=Verbosity.SILENT,
         episode_log=f"{log_dir}/episode.log",
@@ -53,18 +53,18 @@ def make_config(agent_type, threshold):
         win_reward=6000.0,
         scenario_config=PedestriansConfig(
             actors=1,
-            outbound_pavement=1.0,
-            inbound_pavement=1.0
+            outbound_pavement=outbound_pavement,
+            inbound_pavement=inbound_pavement
         ),
-        agent_config=make_agent_config(agent_type, log_dir, threshold=M2PX * threshold),
+        agent_config=make_agent_config(agent_type, log_dir),
         mode_config=HeadlessConfig()
     )
 
 
-def run(agent_type, threshold):
-    print(f"starting: agent_type={agent_type}, threshold={threshold}")
+def run(agent_type, outbound_pavement, inbound_pavement):
+    print(f"starting: agent_type={agent_type}, outbound_pavement={outbound_pavement}, inbound_pavement={inbound_pavement}")
 
-    log_dir, config = make_config(agent_type, threshold)
+    log_dir, config = make_config(agent_type, outbound_pavement, inbound_pavement)
     config.write_json(f"{log_dir}/config.json")
 
     np_seed, env, agents, keyboard_agent = config.setup()
@@ -72,7 +72,7 @@ def run(agent_type, threshold):
     simulation = Simulation(env, agents, config=config, keyboard_agent=keyboard_agent)
     simulation.run()
 
-    print(f"finished: agent_type={agent_type}, threshold={threshold}")
+    print(f"finished: agent_type={agent_type}, outbound_pavement={outbound_pavement}, inbound_pavement={inbound_pavement}")
 
 
 class PoolParser(ArgumentParser):
@@ -93,10 +93,11 @@ class PoolParser(ArgumentParser):
 
 
 if __name__ == '__main__':
-    agent_types = [AgentType.PROXIMITY]
-    thresholds = [i * 0.5 for i in range(1, 100)]
+    agent_types = [AgentType.RANDOM, AgentType.RANDOM_CONSTRAINED, AgentType.PROXIMITY, AgentType.Q_LEARNING]
+    outbound_pavements = [1.0, 0.75, 0.5, 0.25, 0.0]
+    inbound_pavements = [1.0, 0.75, 0.5, 0.25, 0.0]
 
-    parameters = [(agent_type, threshold) for agent_type in agent_types for threshold in thresholds]
+    parameters = [(agent_type, outbound_pavement, inbound_pavement) for agent_type in agent_types for outbound_pavement in outbound_pavements for inbound_pavement in inbound_pavements if outbound_pavement > 0 or inbound_pavement > 0]
 
     parser = PoolParser()
     pool = parser.parse_pool()
