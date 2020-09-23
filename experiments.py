f4cdb900da4f546d6b8f0594c125088a42cca36e
Kevin McAreavey <>
2020-09-23 14:13:47 +0100

    Setup for random(-constrained) experiments
    Added setup for (parallelised) experiments on accuracy of a single random(-constrained) agent relative to epsilon.

--- experiments.py
+++ experiments.py
@@ -1,22 +1,21 @@
 from argparse import ArgumentParser, ArgumentTypeError
 from multiprocessing import Pool
 
-from examples.constants import M2PX
 from reporting import Verbosity
 from config import Config, PedestriansConfig, HeadlessConfig, QLearningConfig, FeatureConfig, AgentType, RandomConfig, \
     RandomConstrainedConfig, ProximityConfig, ElectionConfig
 from simulation import Simulation
 
 
-def make_agent_config(agent_type, log_dir):
+def make_agent_config(agent_type, log_dir, **kwargs):
     if agent_type is AgentType.RANDOM:
-        return RandomConfig(epsilon=0.1)
+        return RandomConfig(**kwargs)
     elif agent_type is AgentType.RANDOM_CONSTRAINED:
-        return RandomConstrainedConfig(epsilon=0.1)
+        return RandomConstrainedConfig(**kwargs)
     elif agent_type is AgentType.PROXIMITY:
-        return ProximityConfig(threshold=M2PX * 22.5)
+        return ProximityConfig(**kwargs)
     elif agent_type is AgentType.ELECTION:
-        return ElectionConfig(threshold=M2PX * 22.5)
+        return ElectionConfig(**kwargs)
     elif agent_type is AgentType.Q_LEARNING:
         return QLearningConfig(
             alpha=0.18,
@@ -36,8 +35,8 @@ def make_agent_config(agent_type, log_dir):
         raise NotImplementedError
 
 
-def make_config(agent_type, outbound_pavement, inbound_pavement):
-    log_dir = f"logs/agent_type={agent_type}/outbound_pavement={outbound_pavement}/inbound_pavement={inbound_pavement}"
+def make_config(agent_type, epsilon):
+    log_dir = f"logs/agent_type={agent_type}/epsilon={epsilon}"
     return log_dir, Config(
         verbosity=Verbosity.SILENT,
         episode_log=f"{log_dir}/episode.log",
@@ -53,18 +52,18 @@ def make_config(agent_type, outbound_pavement, inbound_pavement):
         win_reward=6000.0,
         scenario_config=PedestriansConfig(
             actors=1,
-            outbound_pavement=outbound_pavement,
-            inbound_pavement=inbound_pavement
+            outbound_pavement=1.0,
+            inbound_pavement=1.0
         ),
-        agent_config=make_agent_config(agent_type, log_dir),
+        agent_config=make_agent_config(agent_type, log_dir, epsilon=epsilon),
         mode_config=HeadlessConfig()
     )
 
 
-def run(agent_type, outbound_pavement, inbound_pavement):
-    print(f"starting: agent_type={agent_type}, outbound_pavement={outbound_pavement}, inbound_pavement={inbound_pavement}")
+def run(agent_type, epsilon):
+    print(f"starting: agent_type={agent_type}, epsilon={epsilon}")
 
-    log_dir, config = make_config(agent_type, outbound_pavement, inbound_pavement)
+    log_dir, config = make_config(agent_type, epsilon)
     config.write_json(f"{log_dir}/config.json")
 
     np_seed, env, agents, keyboard_agent = config.setup()
@@ -72,7 +71,7 @@ def run(agent_type, outbound_pavement, inbound_pavement):
     simulation = Simulation(env, agents, config=config, keyboard_agent=keyboard_agent)
     simulation.run()
 
-    print(f"finished: agent_type={agent_type}, outbound_pavement={outbound_pavement}, inbound_pavement={inbound_pavement}")
+    print(f"finished: agent_type={agent_type}, epsilon={epsilon}")
 
 
 class PoolParser(ArgumentParser):
@@ -93,11 +92,10 @@ class PoolParser(ArgumentParser):
 
 
 if __name__ == '__main__':
-    agent_types = [AgentType.RANDOM, AgentType.RANDOM_CONSTRAINED, AgentType.PROXIMITY, AgentType.ELECTION]
-    outbound_pavements = [1.0, 0.75, 0.5, 0.25, 0.0]
-    inbound_pavements = [1.0, 0.75, 0.5, 0.25, 0.0]
+    agent_types = [AgentType.RANDOM, AgentType.RANDOM_CONSTRAINED]
+    epsilons = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
-    parameters = [(agent_type, outbound_pavement, inbound_pavement) for agent_type in agent_types for outbound_pavement in outbound_pavements for inbound_pavement in inbound_pavements if outbound_pavement > 0 or inbound_pavement > 0]
+    parameters = [(agent_type, epsilon) for agent_type in agent_types for epsilon in epsilons]
 
     parser = PoolParser()
     pool = parser.parse_pool()
